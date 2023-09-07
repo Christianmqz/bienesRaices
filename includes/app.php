eece93134dc7642ebaafbd0fa41a8876c0817fea
Christianmqz <christianm.10@hotmail.com>
2023-09-07 23:00:20 +0200

    third commit

--- includes/app.php
+++ includes/app.php
@@ -1,11 +1,15 @@
 <?php 
 
+use Model\ActiveRecord;
+// Permite utilizar las variables de entorno
+require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
 require 'funciones.php';
 require 'config/database.php';
-require __DIR__ . '/../vendor/autoload.php';
+
 
 // Conectarnos a la base de datos
 $db = conectarDB();
-
-use Model\ActiveRecord;
 ActiveRecord::setDB($db);
